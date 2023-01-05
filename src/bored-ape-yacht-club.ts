f5ae382f4c93a5568708d841b296aa87f729de11
aaron-bcw <aaron@bcw.group>
2023-01-05 12:56:54 +0800

    initial commit

---
+++ src/bored-ape-yacht-club.ts
@@ -0,0 +1,28 @@
+import {
+  BoredApeYachtClub as BoredApeYachtClubContract,
+  Transfer as TransferEvent
+} from '../generated/BoredApeYachtClub/BoredApeYachtClub';
+
+import {
+  Ape, User
+} from '../generated/schema';
+
+export function handleTransfer(event: TransferEvent): void {
+  let ape = Ape.load(event.params.tokenId.toString())
+  if (!ape) {
+    ape = new Ape(event.params.tokenId.toString())
+    ape.creator = event.params.to.toHexString();
+    ape.createdAtTimestamp = event.block.timestamp;
+
+    let apeContract = BoredApeYachtClubContract.bind(event.address);
+    ape.contentURI = apeContract.tokenURI(event.params.tokenId);
+  }
+  ape.owner = event.params.to.toHexString();
+  ape.save();
+  
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
